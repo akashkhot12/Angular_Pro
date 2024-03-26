36881fa8cb8883698f6e028ddf2e152a289e3559
akashkhot12 <akashkhot03@gmail.com>
2024-03-26 09:54:46 +0530

    adding component class

---
+++ Ecom-Pro/src/app/product-list/product-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+
+  name:string = "iphone"
+  price:number = 399
+  category:string = "mobile"
+}
