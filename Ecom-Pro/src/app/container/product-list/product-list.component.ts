95e2074fa93ff3c6f55b38f6492a804a799ebaec
akashkhot12 <akashkhot03@gmail.com>
2024-03-26 12:27:30 +0530

    all container clear

--- Ecom-Pro/src/app/container/product-list/product-list.component.ts
+++ Ecom-Pro/src/app/container/product-list/product-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'product-list',
+  selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
