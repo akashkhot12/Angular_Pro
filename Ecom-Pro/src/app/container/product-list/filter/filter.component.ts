931265dd846ec9b85411fa810bf5b3ba4180d983
akashkhot12 <akashkhot03@gmail.com>
2024-03-27 11:12:41 +0530

    inpute decorator

--- Ecom-Pro/src/app/container/product-list/filter/filter.component.ts
+++ Ecom-Pro/src/app/container/product-list/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class FilterComponent {
 
+@Input()
+ all:number = 0;
+
+ @Input()
+ inStocks:number = 0;
+
+ @Input()
+ outOfStocks:number = 0;
 }
